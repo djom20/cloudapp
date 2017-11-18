182ea8b04283ce9cfda8caf9a2b03821d68c907c
Jonathan Olier <jonathan.olier@qrvey.com>
2017-11-18 12:33:07 -0500

    chore(*): remove logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
-console.log(logo);
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
