9ab918bc6f24d4414f6a2256f2b6d17ff1e4b21f
Jonathan Olier <jonathan.olier@qrvey.com>
2017-11-20 17:47:31 -0500

    add login css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import './App.css';
+import { Switch } from 'react-router';
+import { Route } from 'react-router';
+
 import loginBox from './components/loginBox/loginBox';
-// import notFound from './components/notFound/notFound';
+import notFound from './components/notFound/notFound';
+
+import './../node_modules/bootstrap/dist/css/bootstrap.css';
+// import './App.css';
 
 class App extends React.Component {
 	render() {
 		return (
 			<BrowserRouter>
-				<Route exact path='/' component={loginBox} />
+				<Switch>
+					<Route exact path='/' component={loginBox} />
+					<Route component={notFound} />
+				</Switch>
 			</BrowserRouter>
 		);
 	}
--- src/components/loginBox/loginBox.js
+++ src/components/loginBox/loginBox.js
@@ -1,68 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './loginBox.css';
+
+import './loginBox.css';
 
 class loginBox extends React.Component {
 	render() {
 		return (
 			<div id="login" class="row">
-				<div class="col s12 l6 offset-l6">
+				<div class="col col-sm-12 col-lg-6 offset-lg-6">
 					<div class="alt-logo">
-						<img src="/resources/images/brand.png" alt=""></img>
+						<img src="/public/images/brand.png" alt=""></img>
 					</div>
-					<form class="login-form hoverable card-panel z-depth-1">
-						<div class="avatar hide">
-							<div class="rouner">
-								<img ng-model="avatar_img" ng-src="resources/images/user_avatar.png" class="circle" src="resources/images/user_avatar.png"></img>
+					<form class="login-form hoverable card-panel">
+						<div class="row js-fields">
+							<div class="col col-sm-12">
+								<input placeholder="Usuario/Correo" id="username" type="email" class="form-control"></input>
 							</div>
 						</div>
 						<div class="row js-fields">
-							<div class="input-field col s12">
-								<input ng-model="username" id="username" type="email" class="validate"></input>
-								<label for="email">Usuario/Correo</label>
-							</div>
-						</div>
-						<div class="row js-fields">
-							<div class="input-field col s12">
-								<input ng-model="password" type="password" class="validate"></input>
-								<label for="password">Contraseña</label>
+							<div class="col col-sm-12">
+								<input placeholder="Contraseña" type="password" class="form-control"></input>
 							</div>
 						</div>
 						<div class="row center-align js-fields">
-							<div class="input-field col s12">
-								<button ng-click="loginSubmit(username, password)" class="btn waves-effect waves-light light-green" type="submit" name="action">Iniciar Session</button>
+							<div class="input-field col col-sm-12">
+								<button class="btn btn-primary" type="submit" name="action">Iniciar Session</button>
 								<div class="subsection">
 									<a href="#">¿Olvidastes tu contraseña?</a>
 									<hr></hr>
-									<div class="socialicon">
-										<a href="#" class="clearfix">
-											<img src="/resources/images/social/facebook.png" alt=""></img>
+									<div class="socialicon clearfix">
+										<a href="#">
+											<img src="/public/images/social/facebook.png" alt="Facebook"></img>
 										</a>
-										<a href="#" class="clearfix">
-											<img src="/resources/images/social/tweet.png" alt=""></img>
+										<a href="#">
+											<img src="/public/images/social/tweet.png" alt="Tweeter"></img>
 										</a>
-										<a href="#" class="clearfix">
-											<img src="/resources/images/social/in.png" alt=""></img>
+										<a href="#">
+											<img src="/public/images/social/in.png" alt="Linkin"></img>
 										</a>
 									</div>
 								</div>
 							</div>
-						</div>
-						<div class="row preload-login js-preload-login hide">
-							<div class="preloader-wrapper active">
-								<div class="spinner-layer spinner-blue-only">
-									<div class="circle-clipper left">
-										<div class="circle"></div>
-									</div>
-									<div class="gap-patch">
-										<div class="circle"></div>
-									</div>
-									<div class="circle-clipper right">
-										<div class="circle"></div>
-									</div>
-								</div>
-							</div>
-							<label class="loader-text">Conectando...</label>
 						</div>
 					</form>
 					<div class="helper">
